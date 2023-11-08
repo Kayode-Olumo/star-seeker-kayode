287c8c68d1447107c2c8e4e54b0d2d73378f220b
Kayode-Olumo <46356143+Kayode-Olumo@users.noreply.github.com>
2023-11-08 17:55:03 +0000

    implemented better responsivity for nav

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,12 +2,14 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { NavLogo, NavWrapper } from "./styles/NavBar.styles";
 import { SiStarship } from "react-icons/si";
+import { AiFillCloseCircle } from "react-icons/ai";
 import Badge from "@mui/joy/Badge";
 
 const NavBar = ({ favoritesData }) => {
   const navigate = useNavigate();
 
   const [navData, setNavData] = useState();
+  const [isNavOpen, setIsNavOpen] = useState(false);
 
   useEffect(() => {
     setNavData(favoritesData);
@@ -18,9 +20,20 @@ const NavBar = ({ favoritesData }) => {
         <SiStarship className="nav-icon" />
         <NavLogo>starseeker</NavLogo>
       </div>
-      <div className="nav-toggle"></div>
+
+      <div
+        className="nav-toggle"
+        onClick={() => {
+          setIsNavOpen(!isNavOpen);
+        }}
+      >
+        <span />
+        <span />
+        <span />
+      </div>
+
       <div className="nav-links">
-        <ul>
+        <ul className={isNavOpen ? "open" : ""}>
           <li onClick={() => navigate("/")}>Home</li>
           <li onClick={() => navigate("/starseeker/gates/")}>Gates</li>
           <li onClick={() => navigate("/starseeker/cheap-travel")}>
